b3e7b9b5896349baf7fef6f72ad0ed38402417c0
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2025-01-12 18:41:32 +0330

    table design and fetch data

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import HomePage from './components/HomePage'
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./Header.module.css"
+
+function Header() {
+  return (
+    <div className={styles.main}><h1>Crypto App</h1></div>
+  )
+}
+
+export default Header
