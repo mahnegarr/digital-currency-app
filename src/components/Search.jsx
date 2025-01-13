4581376eabed2dbb653872571c520e4e6f8151b2
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2025-01-13 17:08:50 +0330

    search component style added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { searchCoinList } from '../services/cryptoApi';
+import { RotatingLines } from 'react-loader-spinner';
+import styles from "./Search.module.css"
+
 function Search({ crypto, setCrypto }) {
     const [text, setText] = useState("")
     const [coins, setCoins] = useState([])
+    const [isLoading,setIsLoading] = useState(false)
 
     useEffect(() => {
         const controller = new AbortController()
-        if (!text) return;
+        setCoins([])
+        if (!text) {
+            setIsLoading(false)
+            return
+        }
         const searchData = async () => {
 
             try {
                 const res = await fetch(searchCoinList(text), { signal: controller.signal })
                 const json = await res.json()
 
-                if (json.coins) { setCoins(json.coins) } else {
+                if (json.coins) { setIsLoading(false); setCoins(json.coins) } else {
                     alert(json.status.error_message)
                 }
 
@@ -24,18 +32,30 @@ function Search({ crypto, setCrypto }) {
             }
 
         }
+
+        setIsLoading(true)
         searchData()
 
         return () => controller.abort()
     }, [text])
     return (
-        <div>
+        <div className={styles.searchBox}>
             <input type="text" value={text} onChange={e => setText(e.target.value)} />
             <select value={crypto} onChange={e => setCrypto(e.target.value)}>
                 <option value="usd">USD</option>
                 <option value="eur">EUR</option>
                 <option value="jpy">JPY</option>
             </select>
+
+            <div className={styles.searchResult}>
+                {isLoading && <RotatingLines width='50px' height="50px" strokeColor='#f72585' strokeWidth='2'/>}
+                <ul>
+                    {coins.map(coin=> <li key={coin.id} >
+                        <img src={coin.thumb} alt={coin.name} />
+                        <p>{coin.name}</p>
+                    </li>)}
+                </ul>
+            </div>
         </div>
     )
 }
